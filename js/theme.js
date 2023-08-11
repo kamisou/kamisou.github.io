1b95ae78fbf47e5ff105ef5ab14e7afd3fc37fb6
João Marcos Kaminoski de Souza <kamisou.dev@gmail.com>
2023-08-10 22:35:15 -0300

    theme toggle

---
+++ js/theme.js
@@ -0,0 +1,14 @@
+const root = document.querySelector(':root');
+const themeToggle = document.querySelector('#theme-toggle');
+
+themeToggle.addEventListener('mouseup', toggleTheme);
+
+function toggleTheme() {
+    if (root.classList.contains('light-mode')) {
+        root.classList.remove('light-mode');
+        themeToggle.innerText = 'light_mode';
+    } else {
+        root.classList.add('light-mode');
+        themeToggle.innerText = 'dark_mode';
+    }
+}
